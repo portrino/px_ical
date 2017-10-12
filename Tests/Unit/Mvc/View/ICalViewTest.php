05fd4d194cf716816b2e2cc632ec9c67d39e5ea8
Leopold Engst <engst@portrino.de>
2017-10-12 10:44:52 +0200

    * improvfe covverage

--- Tests/Unit/Mvc/View/ICalViewTest.php
+++ Tests/Unit/Mvc/View/ICalViewTest.php
@@ -114,7 +114,43 @@ class ICalViewTest extends UnitTestCase
     /**
      * @test
      */
-    public function renderMultipleEvents()
+    public function renderMultipleEventsAsEvent()
+    {
+        $vEvent1 = new Event();
+        $vEvent1
+            ->setUniqueId('123456')
+            ->setDtStart(new \DateTime('2012-12-24'))
+            ->setDtEnd(new \DateTime('2012-12-24'))
+            ->setDtStamp(new \DateTime('20171012T062640Z'))
+            ->setNoTime(false)
+            ->setSummary('Christmas');
+
+        $vEvent2 = new Event();
+        $vEvent2
+            ->setUniqueId('123456')
+            ->setDtStart(new \DateTime('2012-12-24'))
+            ->setDtEnd(new \DateTime('2012-12-24'))
+            ->setDtStamp(new \DateTime('20171012T062640Z'))
+            ->setNoTime(false)
+            ->setSummary('Eastern');
+
+        $vCalendar = new Calendar(self::$typo3host);
+        $vCalendar->addComponent($vEvent1);
+        $vCalendar->addComponent($vEvent2);
+
+        $this->view->assign('vEvents', [$vEvent1, $vEvent2]);
+
+        $renderedView = $this->view->render();
+
+        $this->view->setPrefix(self::$prefix);
+        $this->view->setOverrideFileName(self::$FileName);
+        static::assertEquals($vCalendar->render(), $renderedView);
+    }
+
+    /**
+     * @test
+     */
+    public function renderMultipleEventsAsBooking()
     {
         $booking1 = new Booking();
         $booking1
