3021352cee9d00dbcb46be6aecf18e02f2947afc
a-wuttig <vagrant@unassigned-hostname.unassigned-domain>
2017-11-11 22:11:59 +0100

    ccodecept debug

--- Tests/Acceptance/tests/_bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__ .'/_support/Helper/Acceptance.php';
-require_once __DIR__ .'/_support/_generated/AcceptanceTesterActions.php';
-require_once __DIR__ .'/_support/AcceptanceTester.php';
